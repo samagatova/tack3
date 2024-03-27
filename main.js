72905ac69fac510286d1d519abdacbd0a48f0606
Nazik <samagatovanazik2@gmail.com>
2024-03-27 21:05:37 +0300

    exercise 1,2

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 // Дан массив ["dlrow", "olleH"]. Измените его таким образом, чтобы в итоге получилось ["Hello", "world"]. Используйте методы массива.
+// !Задание №1
+// let arr = ["dlrow", "olleH"];
 
+// arr = arr.map(a => a.split('').reverse().join(''));
+// arr.reverse();
+// console.log(arr); 
+
+
+// !Задание №2
+// let arr = ['а', 'б', 'в'];
+
+// arr.push('4, 3, 5, 5')
+// let arr2 = arr.join(',')
+// let arr3 = arr2.split(",")
+// console.log(arr3)
 // Дан массив ['а', 'б', 'в']. Добавьте ему в конец элементы 4,3,5,5.
 let arr = ['а', 'б', 'в'];
 arr.push(4, 3, 5, 5);
@@ -8,7 +22,7 @@ console.log(arr);
 // Дан массив ["Apple", "Orange", "Pinapple","Cherry"]
 let arr2 = ["Apple", "Orange", "Pinapple", "Cherry"];
 let firstElement = arr2.shift();
-let pineappleIndex = arr2.indexOf("Pinapple");
+let pineappleIndex = arr2.indexOf("Pinapple"); 
 arr2.splice(pineappleIndex + 1, 0, firstElement);
 console.log(arr2); 
 
